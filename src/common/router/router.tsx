b419e4347f81154c2d144b571f0a0b8e4dc5a228
ula28 <ula24499@gmail.com>
2025-03-11 15:10:27 +0100

    init commit

--- src/common/router/router.tsx
+++ src/common/router/router.tsx
@@ -1,5 +1,5 @@
 import { Page404 } from "common/components"
-import { createBrowserRouter } from "react-router-dom"
+import { createBrowserRouter, createHashRouter } from "react-router-dom"
 import { App } from "../../app/App"
 import { Main } from "../../app/Main"
 import { Login } from "../../features/auth/ui/Login/Login"
@@ -8,7 +8,8 @@ export const Path = {
   Login: "login",
 } as const
 
-export const router = createBrowserRouter([
+
+export const router = createHashRouter([
   {
     path: "/",
     element: <App />,
